af3f53141ebd4f72d409b063e8304c1c787aa58c
rohimoz28 <rohimoz28@gmail.com>
2021-11-24 20:23:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Calculator WEB
+---
+
+[![calculator-js.png](https://i.postimg.cc/qvTsBXKJ/calculator-js.png)](https://postimg.cc/XpsGQ5xm)
+
+Aplikasi berhitung sederhana untuk belajar anak - anak dengan menerapkan pemrograman javascript dasar dan juga javascript Document Objec Model.
---
+++ scripts/add.js
@@ -0,0 +1,60 @@
+// Get Element
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const audio = document.getElementById("myAudio");
+
+let answer = 0;
+
+function generate_equation() {
+  let num1 = Math.floor(Math.random() * 13);
+  let num2 = Math.floor(Math.random() * 13);
+  let dummyAnswer1 = Math.floor(Math.random() * 13);
+  let dummyAnswer2 = Math.floor(Math.random() * 13);
+
+  let allAnswer = [];
+  let switchAnswer = [];
+
+  answer = num1 + num2;
+
+  document.getElementById("num1").innerHTML = num1;
+  document.getElementById("num2").innerHTML = num2;
+
+  allAnswer = [answer, dummyAnswer1, dummyAnswer2];
+
+  for (i = allAnswer.length; i--; ) {
+    switchAnswer.push(
+      allAnswer.splice(Math.floor(Math.random() * (i + 1)), 1)[0]
+    );
+  }
+
+  option1.innerHTML = switchAnswer[0];
+  option2.innerHTML = switchAnswer[1];
+  option3.innerHTML = switchAnswer[2];
+}
+
+option1.addEventListener("click", function () {
+  if (option1.innerHTML == answer) {
+    generate_equation();
+  } else {
+    audio.play();
+  }
+});
+
+option2.addEventListener("click", function () {
+  if (option2.innerHTML == answer) {
+    generate_equation();
+  } else {
+    audio.play();
+  }
+});
+
+option3.addEventListener("click", function () {
+  if (option3.innerHTML == answer) {
+    generate_equation();
+  } else {
+    audio.play();
+  }
+});
+
+generate_equation();
